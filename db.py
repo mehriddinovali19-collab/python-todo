7aae812a8c18ce4816470095637f90fd2c9a5142
djumanov <djumanovdev@gmail.com>
2026-04-29 15:11:28 +0500

    add task

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ import json
 def read_file() -> list[dict]:
     f = open("db.json")
     content = f.read()
-    tasks = json.loads(content)
-    return tasks
+    data = json.loads(content)
+    return data
 
 
 def write_file(tasks: list[dict]):
@@ -15,10 +15,15 @@ def write_file(tasks: list[dict]):
 
 
 def add_task(title: str, description: str):
-    task = {"title": title, "description": description, "status": False}
-    tasks = read_file()
-    tasks.append(task)
-    write_file(tasks)
+    data = read_file()
+    data['tasks'].append({
+        "id": data['current_id'],
+        "title": title, 
+        "description": description, 
+        "status": False
+    })
+    data['current_id'] += 1
+    write_file(data)
 
 
 def mark_task_as_completed():
